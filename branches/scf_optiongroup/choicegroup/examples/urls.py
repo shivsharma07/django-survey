4102db07e40d75cd4c4d3f762caef44e986eeb2c
salimfadhley <salimfadhley@f67e0a98-9c48-0410-a59e-4384d0670f6d>
2009-09-08 21:38:07 +0000

    Made in error

--- branches/scf_optiongroup/choicegroup/examples/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    ('^admin/', include('django.contrib.admin.urls')),
-    (r'',      include('survey.urls')),
-)
